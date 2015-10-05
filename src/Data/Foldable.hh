6197123e9a050a315256427b1e0601502b25bd70
Andy Arvanitis <andyarvanitis@gmail.com>
2015-10-04 22:51:26 -0700

    C++: fix folds

--- src/Data/Foldable.hh
+++ src/Data/Foldable.hh
@@ -22,12 +22,12 @@ namespace Data_Foldable {
   using namespace PureScript;
 
   inline auto foldrArray(const any& f) -> any {
-    return [=](const any& z) -> any {
+    return [=](const any& init) -> any {
       return [=](const any& xs_) -> any {
-        auto xs = xs_.cast<any::vector>();
-        auto acc = z.cast<any::vector>();
-        for (typename any::vector::const_iterator xit = xs.begin(); xit != xs.end() ;++xit) {
-          acc = f(*xit)(acc);
+        const auto& xs = xs_.cast<any::vector>();
+        any acc = init;
+        for (typename any::vector::const_reverse_iterator it = xs.rbegin(); it != xs.rend() ; ++it) {
+          acc = f(*it)(acc);
         }
         return acc;
       };
@@ -35,12 +35,12 @@ namespace Data_Foldable {
   }
 
   inline auto foldlArray(const any& f) -> any {
-    return [=](const any& z) -> any {
+    return [=](const any& init) -> any {
       return [=](const any& xs_) -> any {
-        auto xs = xs_.cast<any::vector>();
-        auto acc = z.cast<any::vector>();
-        for (typename any::vector::const_iterator xit = xs.begin(); xit != xs.end() ;++xit) {
-          acc = f(acc)(*xit);
+        const auto& xs = xs_.cast<any::vector>();
+        any acc = init;
+        for (typename any::vector::const_iterator it = xs.begin(); it != xs.end() ; ++it) {
+          acc = f(acc)(*it);
         }
         return acc;
       };
