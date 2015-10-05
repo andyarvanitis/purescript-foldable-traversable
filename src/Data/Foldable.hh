43c296c1ca5e0e48d0c1879ebc8717d3a4c21513
Andy Arvanitis <andyarvanitis@gmail.com>
2015-10-04 20:46:13 -0700

    C++: update FFI

--- src/Data/Foldable.hh
+++ src/Data/Foldable.hh
@@ -21,12 +21,12 @@ namespace Data_Foldable {
 
   using namespace PureScript;
 
-  template <typename A, typename B>
-  auto foldrArray(const fn<A,fn<B,B>>& f) -> fn<B,fn<array<A>,B>> {
-    return [=](param<B> z) {
-      return [=](const array<A>& xs) {
-        auto acc = z;
-        for (typename array<A>::const_iterator xit = xs.begin(); xit != xs.end() ;++xit) {
+  auto foldrArray(const any& f) -> any {
+    return [=](const any& z) -> any {
+      return [=](const any& xs_) -> any {
+        auto xs = xs_.cast<any::vector>();
+        auto acc = z.cast<any::vector>();
+        for (typename any::vector::const_iterator xit = xs.begin(); xit != xs.end() ;++xit) {
           acc = f(*xit)(acc);
         }
         return acc;
@@ -34,12 +34,12 @@ namespace Data_Foldable {
     };
   }
 
-  template <typename A, typename B>
-  auto foldlArray(const fn<B,fn<A,B>>& f) -> fn<B,fn<array<A>,B>> {
-    return [=](param<B> z) {
-      return [=](const array<A>& xs) {
-        auto acc = z;
-        for (typename array<A>::const_iterator xit = xs.begin(); xit != xs.end() ;++xit) {
+  auto foldlArray(const any& f) -> any {
+    return [=](const any& z) -> any {
+      return [=](const any& xs_) -> any {
+        auto xs = xs_.cast<any::vector>();
+        auto acc = z.cast<any::vector>();
+        for (typename any::vector::const_iterator xit = xs.begin(); xit != xs.end() ;++xit) {
           acc = f(acc)(*xit);
         }
         return acc;
--- src/Data/Traversable.hh
+++ src/Data/Traversable.hh
@@ -21,21 +21,21 @@ namespace Data_Traversable {
 
   using namespace PureScript;
 
-  // forall m a b. (m (a -> b) -> m a -> m b) -> apply
-  //               ((a -> b) -> m a -> m b) -> map
-  //               (a -> m a) -> pure
-  //               (a -> m b) -> f
-  //               Array a -> xs
-  //               m (Array b)
+  // foreign import traverseArrayImpl
+  //   :: forall m a b. (m (a -> b) -> m a -> m b) ->
+  //                    ((a -> b) -> m a -> m b) ->
+  //                    (a -> m a) ->
+  //                    (a -> m b) ->
+  //                    Array a ->
+  //                    m (Array b)
 
   // STUB!!!
-  template <typename A, typename B, template <typename _M1> class M>
-  auto traverseArrayImpl(const fn<M<fn<A,B>>,fn<M<A>,M<B>>>& apply) {
-    return [=](const fn<fn<A,B>,fn<M<A>,M<B>>>& map) {
-      return [=](const fn<A,M<A>>& pure) {
-        return [=](const fn<A,M<B>>& f) {
-          return [=](const array<A>& xs) {
-            return array<B>();
+  auto traverseArrayImpl(const any& apply) -> any {
+    return [=](const any& map) -> any {
+      return [=](const any& pure) -> any {
+        return [=](const any& f) -> any {
+          return [=](const any& xs) -> any {
+            return any::vector();
           };
         };
       };
